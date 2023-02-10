231997f0802d66e6d025a58e81510cea77c026ad
Tuong Nguyen <tuongnm2001@gmail.com>
2023-02-10 14:01:28 +0700

    video 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+    render() {
+        return (
+            <di>Xin chao cac bro </di>
+        )
+    }
+}
+
+export default MyComponents;
