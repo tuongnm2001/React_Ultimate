536b68eeba6f39273c5f8acda80889290f02f57c
Tuong Nguyen <tuongnm2001@gmail.com>
2023-02-11 10:49:32 +0700

    video 38 useEffect Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,42 +1,6 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './DisplayInfor.scss'
 import logo from './../logo.svg'
-
-// class DisplayInfor extends Component {
-
-//     render() {
-
-//         let { listUsers } = this.props
-
-//         return (
-//             <div className='display-infor-container'>
-//                 {
-//                     true &&
-//                     <div>
-//                         {
-//                             listUsers.map((item, index) => {
-//                                 return (
-//                                     <div key={item.id} className={+item.age > 28 ? 'green' : 'red'}>
-//                                         <div>
-//                                             <div>Name : {item.name}</div>
-//                                             <div>Age : {item.age}</div>
-//                                             <div>Address : {item.address}</div>
-//                                         </div>
-
-//                                         <div>
-//                                             <button onClick={() => this.props.handleDeleteUser(item.id)}>Delete</button>
-//                                         </div>
-//                                         <hr />
-//                                     </div>
-//                                 )
-//                             })
-//                         }
-//                     </div>
-//                 }
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfor = (props) => {
     let { listUsers } = props
@@ -46,7 +10,15 @@ const DisplayInfor = (props) => {
         setIsShowHide(!isShowHide)
     }
 
+    useEffect(() => {
+        if (listUsers.length === 0) {
+            alert('User Empty')
+        }
+        console.log('call useEffect');
+    }, [listUsers])
+
     return (
+
         <div className='display-infor-container'>
             <div>
                 <span onClick={() => { handleShowHideUser() }} style={{ cursor: 'pointer' }}>{isShowHide === true ? 'Hide list Users ' : 'Show list Users '}</span>
