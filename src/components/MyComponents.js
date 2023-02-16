f43be3de2ba754a082014fbfc8cefb43ece20314
Tuong Nguyen <tuongnm2001@gmail.com>
2023-02-16 09:44:46 +0700

    change file adduser

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -2,47 +2,6 @@ import React, { useState } from "react";
 import DisplayInfor from "./DisplayInfor";
 import AddUserInfor from "./AddUserInfor";
 
-// class MyComponents extends React.Component {
-
-//     state = {
-//         listUsers: [
-//             { id: 1, name: 'Nguyen Van A ', age: 28, address: 'Ha Noi' },
-//             { id: 2, name: 'Nguyen Van B ', age: 29, address: 'TP Ho Chi Minh' },
-//             { id: 3, name: 'Nguyen Van C ', age: 30, address: 'Can Tho' },
-//         ]
-
-//     }
-
-//     handleAddNewUser = (objUser) => {
-//         this.setState({
-//             listUsers: [objUser, ...this.state.listUsers]
-//         })
-//     }
-
-//     handleDeleteUser = (userId) => {
-//         let listUsersClone = this.state.listUsers;
-//         listUsersClone = listUsersClone.filter(item => item.id !== userId)
-//         this.setState({
-//             listUsers: listUsersClone
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <AddUserInfor
-//                     handleAddNewUser={this.handleAddNewUser}
-//                 />
-//                 <br /><br />
-//                 <DisplayInfor
-//                     listUsers={this.state.listUsers}
-//                     handleDeleteUser={this.handleDeleteUser}
-//                 />
-//             </div>
-//         )
-//     }
-// }
-
 const MyComponents = () => {
 
     const [listUsers, setListUsers] = useState(
