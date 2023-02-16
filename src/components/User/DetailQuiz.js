22a8771c13baee1105c0764612366c9fa2a7f485
Tuong Nguyen <tuongnm2001@gmail.com>
2023-02-16 22:53:44 +0700

    process data , handle data Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../../service/apiService"
+import _ from 'lodash'
 
 const DetailQuiz = (props) => {
 
@@ -13,7 +14,32 @@ const DetailQuiz = (props) => {
 
     const fetchQuestion = async () => {
         let res = await getDataQuiz(quizId);
-        console.log('check questions : ', res);
+
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null
+
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                        answers.push(item.answers)
+                    })
+
+                    return {
+                        questionId: key,
+                        answers, questionDescription, image
+                    }
+                })
+                .value()
+        }
     }
 
     return (
