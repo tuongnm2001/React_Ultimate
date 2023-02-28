f48fb435864fe041a73cd648eab26074b3d19a4d
Tuong Nguyen <tuongnm2001@gmail.com>
2023-02-28 14:47:50 +0700

    test

--- src/App.js
+++ src/App.js
@@ -10,29 +10,9 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div>Hello World1</div>
         <MyComponents />
       </>
     )
   }
 }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello World
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,37 +1,14 @@
 import React, { useState } from "react";
-import DisplayInfor from "./DisplayInfor";
-import AddUserInfor from "./AddUserInfor";
+import TableUser from "./TableUser";
+
 
 const MyComponents = () => {
 
-    const [listUsers, setListUsers] = useState(
-        [
-            { id: 1, name: 'Nguyen Van A ', age: 28, address: 'Ha Noi' },
-            { id: 2, name: 'Nguyen Van B ', age: 29, address: 'TP Ho Chi Minh' },
-            { id: 3, name: 'Nguyen Van C ', age: 30, address: 'Can Tho' },
-        ]
-    )
 
-    const handleAddNewUser = (objUser) => {
-        setListUsers([objUser, ...listUsers])
-    }
-
-    const handleDeleteUser = (userId) => {
-        let listUsersClone = listUsers;
-        listUsersClone = listUsersClone.filter(item => item.id !== userId)
-        setListUsers(listUsersClone)
-    }
 
     return (
         <div>
-            <AddUserInfor
-                handleAddNewUser={handleAddNewUser}
-            />
-            <br /><br />
-            <DisplayInfor
-                listUsers={listUsers}
-                handleDeleteUser={handleDeleteUser}
-            />
+            <TableUser />
         </div>
     )
 }
